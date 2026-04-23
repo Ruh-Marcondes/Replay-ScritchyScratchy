0aa74a03e7e861eee787a366d7cbfa2d96fb27fc
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2026-04-23 11:08:18 -0300

    Moldando a logica

--- src/main/java/com/ruhmarcondes/game/Main.java
+++ src/main/java/com/ruhmarcondes/game/Main.java
@@ -1,10 +1,38 @@
 package com.ruhmarcondes.game;
 
+import java.util.Scanner;
+
 import com.ruhmarcondes.game.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Upgrades op = new Upgrades();
+        
+        Scanner sc = new Scanner(System.in);
+
+        
+        Bestupgrade upgrade = new Bestupgrade();
+        Player p1 = new Player(3);
+
+    do {
+        //game logica
+        switch (Integer.parseInt(sc.nextLine())) {
+            case 1:
+                    //raspar
+                break;
+            case 2:
+                //upgradar 
+                if (Integer.parseInt(sc.nextLine()) == 10) {
+                    //Upgrade luck
+                }
+                break;
+            default:
+                break;
+        }
+
+    } while (Integer.parseInt(sc.nextLine())!= 0);
+    sc.close();
+    System.out.println("Saindo do game");
+
     }
 }
--- src/main/java/com/ruhmarcondes/game/Player.java
+++ src/main/java/com/ruhmarcondes/game/Player.java
@@ -4,7 +4,16 @@ public class Player {
     
     int money;
 
+    public int getMoney() {
+        return this.money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
     Player(int money){
         this.money = money;
     }
+
 }
